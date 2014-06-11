1199f48d28cfe878373046e9c25fe3b20976afd4
Lex Tang <lexrus@gmail.com>
2014-06-11 18:07:53 +0800

    Initial commit.

---
+++ LTBouncyPlaceholder/UITextField+LTBouncyPlaceholderKeys.swift
@@ -0,0 +1,16 @@
+//
+//  UITextField+LTBouncyPlaceholderKeys.swift
+//  LTBouncyPlaceholderDemo
+//
+//  Created by Lex on 6/10/14.
+//  Copyright (c) 2014 LexTang.com. All rights reserved.
+//
+
+var kAlwaysBouncePlaceholderKey = "kAlwaysBouncePlaceholder"
+let kAlwaysBouncePlaceholderPointer: CConstVoidPointer = &kAlwaysBouncePlaceholderKey
+var kAbbreviatedPlaceholderKey = "kAbbreviatedPlaceholderKey"
+let kAbbreviatedPlaceholderPointer: CConstVoidPointer = &kAbbreviatedPlaceholderKey
+var kPlaceholderLabelKey = "kPlaceholderLabelKey"
+let kPlaceholderLabelPointer: CConstVoidPointer = &kPlaceholderLabelKey
+var kRightPlaceholderLabelKey = "kRightPlaceholderLabelKey"
+let kRightPlaceholderLabelPointer: CConstVoidPointer = &kRightPlaceholderLabelKey
---
+++ LTBouncyPlaceholderDemo/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  LTBouncyPlaceholderDemo
+//
+//  Created by Lex on 6/9/14.
+//  Copyright (c) 2014 LexTang.com. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+                            
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: NSDictionary?) -> Bool {
+        self.window!.backgroundColor = UIColor.whiteColor()
+        return true
+    }
+
+
+}
+
